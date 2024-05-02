554981a8f57994342c14d65b3be2db2462927899
jesusbeb <jesusbelb@gmail.com>
2024-05-02 17:43:48 -0600

    Creando clase Principal y menu

--- src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
+++ src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
@@ -1,13 +1,22 @@
 package com.aluracursos.screenmatch;
 
+import com.aluracursos.screenmatch.principal.Principal;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Hacemos uso de la interfaz CommandLineRunner que permite realizar una accion cuando
+//iniciemos la app, p.e. puede ser un mensaje en consola, cargar una BD, etc...
 @SpringBootApplication
-public class ScreenmatchApplication {
+public class ScreenmatchApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenmatchApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Principal principal = new Principal();
+		principal.muestraElMenu();
+	}
 }
---
+++ src/main/java/com/aluracursos/screenmatch/principal/Principal.java
@@ -0,0 +1,20 @@
+package com.aluracursos.screenmatch.principal;
+
+import java.util.Scanner;
+
+public class Principal {
+    //variables
+    private Scanner teclado = new Scanner(System.in);
+
+    //metodos
+    public void muestraElMenu(){
+        System.out.println("Escribe el nombre de la serie que deseas buscar: ");
+        String buscarSerie = teclado.nextLine();
+
+
+
+
+        
+
+    }
+}
